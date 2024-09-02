db0181984ca7b26d484d6f5a70889aa98aa0105b
alenash1988 <Aleonash1988@mail.ru>
2024-09-02 20:10:49 +0900

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def divide(first, second):
+    if second == 0:
+        return print('Ошибка')
+    resul = first / second
+    print(resul)
+
+
+
+
---
+++ true_math.py
@@ -0,0 +1,10 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return print(inf)
+    result = first / second
+    print(result)
+
+
+
+
